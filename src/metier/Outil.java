98f6ce77c7682bd8378cbca9bf031134c00ffdff
Piki <alexandradesplan@gmail.com>
2023-03-21 20:07:12 +0100

    ajout d'une factory + ajout du package

--- src/metier/Outil.java
+++ src/metier/Outil.java
@@ -1,14 +1,31 @@
+package metier;
 
 public class Outil
 {
-	private static String[] OUTIL_DIPONIBLE = {"Cercle","Rectangle","Ligne","Texte","Remplir"};
-	private static String[] COULEURS_DISPONIBLE = {"Red","Bleu","Green","Yellow","Black","White"};
-
+	private final static String[] OUTIL_DIPONIBLE     = {"Cercle","Rectangle","Ligne","Texte","Remplir"};
+	private final static String[] COULEURS_DISPONIBLE = {"Red","Bleu","Green","Yellow","Black","White" };
 
 	private String outil;
 	private String libelle;
 	private String couleur;
 
+	public static Outil creerOutil(String outil, String libelle, String couleur)
+	{
+		boolean contient = false;
+		for(int cpt = 0; cpt < Outil.OUTIL_DIPONIBLE.length;cpt++)
+			if(Outil.OUTIL_DIPONIBLE[cpt] == outil)
+				contient = true;
+
+		if(!contient)
+			return null;
+
+		return new Outil(outil, libelle, couleur);
+	}
+
+	public static Outil creerOutil(String outil, String couleur)
+	{
+		return creerOutil(outil, "", couleur);
+	}
 
 	public Outil(String outil, String libelle, String couleur)
 	{
@@ -17,14 +34,9 @@ public class Outil
 		this.couleur = couleur;
 	}
 
-	public Outil(String outil, String libelle)
+	public Outil(String outil, String couleur)
 	{
-		this(outil, libelle, "Black");
-	}
-
-	public Outil(String outil)
-	{
-		this(outil, "" , "Black");
+		this(outil, "", couleur);
 	}
 
 	public boolean setCouleur(String couleur)
@@ -39,12 +51,10 @@ public class Outil
 		return false;
 	}
 
-	public String getLibelle(){return this.libelle;}
+	public String getLibelle(){return this.libelle; }
+	public String getOutil	(){return this.outil;	}
+	public String getCouleur(){return this.couleur; }
 
-	public String getOutil(){return this.outil;}
-
-	public String getCouleur(){return this.couleur;}
-	
 	public String toString()
 	{
 		return "Outil selectionne" + this.outil + "de couleurs" + this.couleur;
