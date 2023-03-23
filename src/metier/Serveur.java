579bd7a67b96f89529a867599c7817b016feae92
Piki <alexandradesplan@gmail.com>
2023-03-23 08:56:53 +0100

    serveur + controleur

---
+++ src/metier/Serveur.java
@@ -0,0 +1,35 @@
+package metier;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Serveur 
+{
+    private ServerSocket serverSocket;
+    private  int port;
+
+    public Serveur(int port)
+    {
+        this.port = port;
+
+        try {
+            
+            this.serverSocket = new ServerSocket(this.port);
+            System.out.println("Serveur en ecoute sur le port " + this.port);
+
+            while(true)
+            {
+                Socket socket = this.serverSocket.accept();
+                System.out.println("Nouvelle connexion");
+                Thread t = new Thread(new Service(socket));
+                t.start();
+            }
+
+
+        } catch (IOException e ) {e.printStackTrace();}
+
+    }
+
+
+}
