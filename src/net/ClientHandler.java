b7a42f115056a7e6ca9d0149fd93fb9a3395da63
Piki <alexandradesplan@gmail.com>
2023-03-24 12:39:47 +0100

    mise a jour du ClientHandler comme avant

--- src/net/ClientHandler.java
+++ src/net/ClientHandler.java
@@ -1,17 +1,10 @@
 package net;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-
-import ihm.FrameDessin;
-import ihm.PanelDessin;
 
 public class ClientHandler implements Runnable 
 {
@@ -41,14 +34,15 @@ public class ClientHandler implements Runnable
     {
         try {
 
-            //Attendre qu'un client modifie le panelDessin
-            while (true) 
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) 
             {
-                // Récupérer le panelDessin envoyé par le client
-                PanelDessin panelDessin = (PanelDessin) in.readObject();
-                server.sendPanelDessin(panelDessin);
+                // Lire les messages envoyés par le client
+                System.out.println("Message reçu de " + clientSocket + " : " + inputLine);
+                // Envoyer le message à tous les autres clients connectés
+                server.broadcast(inputLine);
             }
-            
             // Le client s'est déconnecté
             server.removeClient(this);
             in.close();
@@ -67,32 +61,8 @@ public class ClientHandler implements Runnable
         out.println(message);
     }
 
-    public void sendPanelDessin(PanelDessin panelDessin) 
-    {
-        try {
-
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-            objectOutputStream.writeObject(panelDessin);
-            byte[] data = byteArrayOutputStream.toByteArray();
-    
-            // Envoi du tableau de bytes contenant le panelDessin au client
-            OutputStream outputStream = clientSocket.getOutputStream();
-            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-            
-            dataOutputStream.writeInt(data.length);
-            dataOutputStream.write(data);
-            dataOutputStream.flush();
-            
-        } catch (IOException e) {
-            System.err.println("Erreur lors de l'envoi du panelDessin au client.");
-            e.printStackTrace();
-        }
-    }
-
     public Socket getClientSocket() 
     {
         return this.clientSocket;
     }
-
 }
